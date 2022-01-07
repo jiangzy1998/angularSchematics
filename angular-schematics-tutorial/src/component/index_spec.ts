43d102c0fb0fe29356c0f9f5582c1c0a3e824215
jiangzy <jiangzy1998@gmail.com>
2022-01-07 18:13:52 +0800

    [SCHEMATICS]原理图-实现component-2

--- angular-schematics-tutorial/src/component/index_spec.ts
+++ angular-schematics-tutorial/src/component/index_spec.ts
@@ -1,8 +1,26 @@
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { Schema as Workspaceoptions } from "@schematics/angular/workspace/schema";
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
+
+
+describe("component", ()=>{
+
+  // 选项 --name
+  const name = 'apple';
+  const runner = new SchematicTestRunner("schematics", collectionPath);
+
+  // 模拟 ng new 创建angular项目，主要对 workspace 和 application 进行配置
+
+  // angular 项目对配置
+  const workspaceoptions:Workspaceoptions = {
+    name:'wokspace',
+    newProjectRoot:"projects",
+    version:"6.0.0",
+  };
+})
 
 describe('angular-schematics-tutorial', () => {
   it('works', async () => {
