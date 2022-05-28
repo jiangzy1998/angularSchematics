92e6dff4f1e4ea0b45f738baedd494b72c92023e
jiangzy <jiangzy1998@gmail.com>
2022-05-28 10:20:58 +0800

    feat:angular 支持

--- schematics30day/sample03/hello-world/src/hello-world/index.ts
+++ schematics30day/sample03/hello-world/src/hello-world/index.ts
@@ -1,12 +1,34 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { strings } from '@angular-devkit/core'; // 引入 strings， 所有对字符串处理函数都在里面
+import { apply, mergeWith, move, Rule, SchematicContext, SchematicsException, template, Tree, url } from '@angular-devkit/schematics';
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { buildDefaultPath } from "@schematics/angular/utility/workspace";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function helloWorld(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    // 新建一个文件，文件名为用户输入对 --name 的参数，如果没有输入则使用默认的“hello”，文件的内容为 world；
-    tree.create(_options.name || "hello", "world");
-    return tree;
+  return (_tree: Tree, _context: SchematicContext) => {
+    const workspaceConfigBuffer = _tree.read('angular.json');
+    if(!workspaceConfigBuffer){
+      throw new SchematicsException("Not an Angular CLI workspace");
+    }
+
+    const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+    const projectName = _options.project || workspaceConfig.defaultProject;
+
+    const project = workspaceConfig.projects[projectName];
+    const defaultProjectPath = buildDefaultPath(project);
+    const parsePath = parseName(defaultProjectPath, _options.name);
+    const { name, path } = parsePath;
+
+    const sourceTemplates = url('./files');
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options, // 使用者所輸入的參數
+        ...strings,  // 将这些函数添加到规则里，模版语法才能正确运作
+        name
+      }),
+      move(path) // 将生产出来的档案移动到目录下
+    ]);
+    return mergeWith(sourceParametrizedTemplates);
   };
 }
--- schematics30day/sample03/hello-world/src/hello-world/index_spec.ts
+++ schematics30day/sample03/hello-world/src/hello-world/index_spec.ts
@@ -1,3 +1,4 @@
+import { strings } from '@angular-devkit/core';
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
@@ -5,20 +6,21 @@ import * as path from 'path';
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('hello-world', () => {
-  
-  const expectResult = async (fileName?:string) => {
-    const fullFileName = `/${fileName || 'hello'}`;
-    const params = fileName ? { name:fileName } : {};
+  const expectResult = async (fileName:string) => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematicAsync('hello-world', params, Tree.empty()).toPromise();
+    const tree = await runner.runSchematicAsync('hello-world', { name:fileName }, Tree.empty()).toPromise();
+
+    const dasherizeName = strings.dasherize(fileName);
+    const fullFileName = `/hello-${dasherizeName}.component.ts`;
     expect(tree.files).toContain(fullFileName);
-    expect(tree.readContent(fullFileName)).toEqual("world");
+
+    const fileContent = tree.readContent(fullFileName);
+    expect(fileContent).toMatch(/hello-jiangzy/);
+    expect(fileContent).toMatch(/HelloJiangzyComponent/);
   }
 
-  it("使用者没有给文件名，应该使用默认文件名\'hello\'", ()=>{
-    expectResult();
-  })
-  it("使用者有给文件名，应该使用使用者输入的文件名", ()=>{
+  it('成功生成文档名为"/hello-jiangzy.component.ts"', ()=>{
     expectResult("jiangzy");
   })
+ 
 });
